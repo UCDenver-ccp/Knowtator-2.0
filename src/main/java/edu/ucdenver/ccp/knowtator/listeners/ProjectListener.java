4aeb260dccc1787f3058d2cd24e1db88fc7660e5
tuh8888 <pielkekid@gmail.com>
2018-03-07 00:36:42 -0700

    Removed target again

--- src/main/java/edu/ucdenver/ccp/knowtator/listeners/ProjectListener.java
+++ src/main/java/edu/ucdenver/ccp/knowtator/listeners/ProjectListener.java
@@ -22,47 +22,8 @@
  * SOFTWARE.
  */
 
-package edu.ucdenver.ccp.knowtator.view.graph;
+package edu.ucdenver.ccp.knowtator.listeners;
 
-import edu.ucdenver.ccp.knowtator.view.KnowtatorIcons;
-import org.apache.log4j.Logger;
-
-import javax.swing.*;
-
-class GraphToolBar extends JToolBar {
-
-    @SuppressWarnings("unused")
-    private static final Logger log = Logger.getLogger(GraphToolBar.class);
-
-    private GraphViewer graphViewer;
-
-    GraphToolBar(GraphViewer graphViewer) {
-
-        this.graphViewer = graphViewer;
-
-        setFloatable(false);
-
-        add(addGraphNodeCommand());
-        add(removeGraphNodeCommand());
-    }
-
-    private JButton removeGraphNodeCommand() {
-        JButton button = new JButton(KnowtatorIcons.getIcon(KnowtatorIcons.REMOVE));
-        button.setToolTipText("Remove node");
-
-        button.addActionListener(e -> graphViewer.removeSelectedVertex());
-
-        return button;
-    }
-
-    private JButton addGraphNodeCommand() {
-        JButton button = new JButton(KnowtatorIcons.getIcon(KnowtatorIcons.ADD));
-        button.setToolTipText("Add annotation as node");
-
-        button.addActionListener(e -> graphViewer.addSelectedAnnotationAsVertex());
-
-        return button;
-    }
-
-
+public interface ProjectListener {
+    void projectLoaded();
 }
