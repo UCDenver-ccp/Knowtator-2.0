e84926f42c97dba26304e8809fcc1d8bb1f31f39
tuh8888 <harrison.pielke-lombardo@ucdenver.edu>
2018-05-28 17:26:15 -0600

    Centered graph space

--- src/main/java/edu/ucdenver/ccp/knowtator/view/GraphView.java
+++ src/main/java/edu/ucdenver/ccp/knowtator/view/GraphView.java
@@ -306,7 +306,7 @@ public class GraphView extends JPanel implements GraphSpaceSelectionListener, Pr
 		propertyValuePanel.add(label2, new com.intellij.uiDesigner.core.GridConstraints(2, 1, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_WEST, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
 		final JPanel panel4 = new JPanel();
 		panel4.setLayout(new BorderLayout(0, 0));
-		panel1.add(panel4, BorderLayout.SOUTH);
+		panel1.add(panel4, BorderLayout.CENTER);
 		graphComponent.setCenterPage(false);
 		graphComponent.setGridVisible(true);
 		panel4.add(graphComponent, BorderLayout.CENTER);
