748a62000be4b6c3b59e5d837ebf0225e6b9d969
tuh8888 <pielkekid@gmail.com>
2018-08-14 13:47:50 -0600

    Set default profile as selected profile on init

--- src/main/java/edu/ucdenver/ccp/knowtator/model/ProfileManager.java
+++ src/main/java/edu/ucdenver/ccp/knowtator/model/ProfileManager.java
@@ -20,94 +20,100 @@ import java.util.Map;
 
 public class ProfileManager implements Savable {
 
-  private static final Logger log = Logger.getLogger(KnowtatorController.class);
+    private static final Logger log = Logger.getLogger(KnowtatorController.class);
 
-  private ProfileCollection profileCollection;
-  private KnowtatorController controller;
-  private List<ColorListener> colorListeners;
+    private ProfileCollection profileCollection;
+    private KnowtatorController controller;
+    private List<ColorListener> colorListeners;
 
-  public ProfileManager(KnowtatorController controller) {
-    this.controller = controller;
-    colorListeners = new ArrayList<>();
-    profileCollection = new ProfileCollection(controller);
-  }
-
-  public void addColorListener(ColorListener listener) {
-    colorListeners.add(listener);
-  }
-
-  public Profile addProfile(String profileID) {
-    Profile newProfile = profileCollection.get(profileID);
-    if (newProfile == null) {
-      newProfile = new Profile(controller, profileID);
-      profileCollection.add(newProfile);
+    public ProfileManager(KnowtatorController controller) {
+        this.controller = controller;
+        colorListeners = new ArrayList<>();
+        profileCollection = new ProfileCollection(controller);
+        controller.getSelectionManager().setSelectedProfile(getDefaultProfile());
     }
-    controller.getSelectionManager().setSelectedProfile(newProfile);
-    return newProfile;
-  }
 
-  private void removeProfile(Profile profile) {
-    profileCollection.remove(profile);
-    controller.getSelectionManager().setSelectedProfile(profileCollection.iterator().next());
-  }
-
-  public ProfileCollection getProfileCollection() {
-    return profileCollection;
-  }
-
-  public void writeToKnowtatorXML(Document dom, Element root) {
-    profileCollection.forEach(profile -> profile.writeToKnowtatorXML(dom, root));
-  }
-
-  @Override
-  public void readFromKnowtatorXML(File file, Element parent) {
-    for (Node profileNode :
-        KnowtatorXMLUtil.asList(parent.getElementsByTagName(KnowtatorXMLTags.PROFILE))) {
-      Element profileElement = (Element) profileNode;
-      String profileID = profileElement.getAttribute(KnowtatorXMLAttributes.ID);
-
-      Profile newProfile = addProfile(profileID);
-      newProfile.readFromKnowtatorXML(null, profileElement);
+    public void addColorListener(ColorListener listener) {
+        colorListeners.add(listener);
     }
-  }
 
-  @Override
-  public void readFromOldKnowtatorXML(File file, Element parent) {}
-
-  @Override
-  public void readFromBratStandoff(
-      File file, Map<Character, List<String[]>> annotationMap, String content) {}
-
-  @SuppressWarnings("RedundantThrows")
-  @Override
-  public void writeToBratStandoff(Writer writer, Map<String, Map<String, String>> annotationsConfig, Map<String, Map<String, String>> visualConfig) throws IOException {}
-
-  @Override
-  public void readFromGeniaXML(Element parent, String content) {}
-
-  @Override
-  public void writeToGeniaXML(Document dom, Element parent) {}
-
-  public Profile getDefaultProfile() {
-    return profileCollection.getDefaultProfile();
-  }
-
-  public void removeActiveProfile() {
-    if (controller.getSelectionManager().getActiveProfile() != profileCollection.getDefaultProfile()) {
-      removeProfile(controller.getSelectionManager().getActiveProfile());
+    public Profile addProfile(String profileID) {
+        Profile newProfile = profileCollection.get(profileID);
+        if (newProfile == null) {
+            newProfile = new Profile(controller, profileID);
+            profileCollection.add(newProfile);
+        }
+        controller.getSelectionManager().setSelectedProfile(newProfile);
+        return newProfile;
     }
-  }
 
-  public Profile getProfile(String profileID) {
-    return profileCollection.get(profileID);
-  }
+    private void removeProfile(Profile profile) {
+        profileCollection.remove(profile);
+        controller.getSelectionManager().setSelectedProfile(profileCollection.iterator().next());
+    }
 
-  void fireColorChanged() {
-    colorListeners.forEach(ColorListener::colorChanged);
-  }
+    public ProfileCollection getProfileCollection() {
+        return profileCollection;
+    }
 
-  public void dispose() {
-    profileCollection.getCollection().clear();
-    colorListeners.clear();
-  }
+    public void writeToKnowtatorXML(Document dom, Element root) {
+        profileCollection.forEach(profile -> profile.writeToKnowtatorXML(dom, root));
+    }
+
+    @Override
+    public void readFromKnowtatorXML(File file, Element parent) {
+        for (Node profileNode :
+                KnowtatorXMLUtil.asList(parent.getElementsByTagName(KnowtatorXMLTags.PROFILE))) {
+            Element profileElement = (Element) profileNode;
+            String profileID = profileElement.getAttribute(KnowtatorXMLAttributes.ID);
+
+            Profile newProfile = addProfile(profileID);
+            newProfile.readFromKnowtatorXML(null, profileElement);
+        }
+    }
+
+    @Override
+    public void readFromOldKnowtatorXML(File file, Element parent) {
+    }
+
+    @Override
+    public void readFromBratStandoff(
+            File file, Map<Character, List<String[]>> annotationMap, String content) {
+    }
+
+    @SuppressWarnings("RedundantThrows")
+    @Override
+    public void writeToBratStandoff(Writer writer, Map<String, Map<String, String>> annotationsConfig, Map<String, Map<String, String>> visualConfig) throws IOException {
+    }
+
+    @Override
+    public void readFromGeniaXML(Element parent, String content) {
+    }
+
+    @Override
+    public void writeToGeniaXML(Document dom, Element parent) {
+    }
+
+    public Profile getDefaultProfile() {
+        return profileCollection.getDefaultProfile();
+    }
+
+    public void removeActiveProfile() {
+        if (controller.getSelectionManager().getActiveProfile() != profileCollection.getDefaultProfile()) {
+            removeProfile(controller.getSelectionManager().getActiveProfile());
+        }
+    }
+
+    public Profile getProfile(String profileID) {
+        return profileCollection.get(profileID);
+    }
+
+    void fireColorChanged() {
+        colorListeners.forEach(ColorListener::colorChanged);
+    }
+
+    public void dispose() {
+        profileCollection.getCollection().clear();
+        colorListeners.clear();
+    }
 }
