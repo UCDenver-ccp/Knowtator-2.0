ad9d35a011ec1ac719e2113729d3a01f4d077323
tuh8888 <pielkekid@gmail.com>
2018-10-29 13:02:40 -0600

    Added undo manager

--- src/main/java/edu/ucdenver/ccp/knowtator/ProjectManager.java
+++ src/main/java/edu/ucdenver/ccp/knowtator/ProjectManager.java
@@ -17,7 +17,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public abstract class ProjectManager extends DebugManager implements Savable {
+public abstract class ProjectManager extends UndoManager implements Savable {
     @SuppressWarnings("unused")
     private static final Logger log = Logger.getLogger(ProjectManager.class);
 
