bf98bfe2a583a08b896530711f801fe5538c9ece
tuh8888 <pielkekid@gmail.com>
2018-11-07 20:57:42 -0700

    Started moving IO tests to project manager tests

---
+++ src/test/java/edu/ucdenver/ccp/knowtator/ProjectManagerTest.java
@@ -0,0 +1,37 @@
+package edu.ucdenver.ccp.knowtator;
+
+import org.junit.Test;
+
+public class ProjectManagerTest {
+
+    private static final KnowtatorController controller = TestingHelpers.getLoadedController();
+
+    @Test
+    public void loadProject() {
+        TestingHelpers.checkDefaultCollectionValues(controller);
+    }
+
+    @Test
+    public void newProject() {
+    }
+
+    @Test
+    public void importToManager() {
+    }
+
+    @Test
+    public void importProject() {
+    }
+
+    @Test
+    public void makeProjectStructure() {
+    }
+
+    @Test
+    public void loadWithAppropriateFormat() {
+    }
+
+    @Test
+    public void saveToFormat() {
+    }
+}
