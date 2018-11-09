30db3921ff96c8f29ca6392e211f68164eee4fe1
tuh8888 <pielkekid@gmail.com>
2018-11-08 19:18:07 -0700

    Added documentation for Knowtator controller

--- src/main/java/edu/ucdenver/ccp/knowtator/KnowtatorController.java
+++ src/main/java/edu/ucdenver/ccp/knowtator/KnowtatorController.java
@@ -49,139 +49,209 @@ import java.util.stream.Collectors;
  * @author Harrison Pielke-Lombardo
  */
 public class KnowtatorController extends ProjectManager implements KnowtatorObjectInterface {
-    @SuppressWarnings("unused")
-    private static final Logger log = Logger.getLogger(KnowtatorController.class);
-    private final List<BaseKnowtatorModel> models;
-    private final SelectionModel selectionModel;
-    private final FilterModel filterModel;
-    private final TextSourceCollection textSourceCollection;
-    private final ProfileCollection profileCollection;
-    private final OWLModel owlModel;
-    private final TreeMap<String, KnowtatorDataObjectInterface> idRegistry;
+	@SuppressWarnings("unused")
+	private static final Logger log = Logger.getLogger(KnowtatorController.class);
+	private final List<BaseKnowtatorModel> models;
+	private final SelectionModel selectionModel;
+	private final FilterModel filterModel;
+	private final TextSourceCollection textSourceCollection;
+	private final ProfileCollection profileCollection;
+	private final OWLModel owlModel;
+	private final TreeMap<String, KnowtatorDataObjectInterface> idRegistry;
 
 
-    public KnowtatorController() {
-        super();
-        idRegistry = new TreeMap<>();
+	/**
+	 * The constructor initializes all of the models and managers
+	 */
+	public KnowtatorController() {
+		super();
 
-        models = new ArrayList<>();
-        textSourceCollection = new TextSourceCollection(this);
-        owlModel = new OWLModel(this);
-        profileCollection = new ProfileCollection(this); // manipulates profiles and colors
-        filterModel = new FilterModel();
-        selectionModel = new SelectionModel();
+		idRegistry = new TreeMap<>();
 
-        models.add(owlModel);
-        models.add(profileCollection);
-        models.add(textSourceCollection);
-        models.add(filterModel);
-        models.add(selectionModel);
-    }
+		models = new ArrayList<>();
+		textSourceCollection = new TextSourceCollection(this);
+		owlModel = new OWLModel(this);
+		profileCollection = new ProfileCollection(this);
+		filterModel = new FilterModel();
+		selectionModel = new SelectionModel();
 
-    @Override
-    List<BaseKnowtatorManager> getManagers() {
-        return models.stream().filter(model -> model instanceof BaseKnowtatorManager).map(model -> (BaseKnowtatorManager) model).collect(Collectors.toList());
-    }
+		models.add(owlModel);
+		models.add(profileCollection);
+		models.add(textSourceCollection);
+		models.add(filterModel);
+		models.add(selectionModel);
+	}
 
-    public FilterModel getFilterModel() {
-        return filterModel;
-    }
+	/**
+	 * @param args Unused
+	 */
+	public static void main(String[] args) {
+		log.warn("Knowtator");
+	}
 
-    void importProject(File profilesLocation, File ontologiesLocation, File articlesLocation, File annotationsLocation, File projectLocation) {
-        try {
-            setSaveLocation(projectLocation);
-            makeProjectStructure();
-            importToManager(profilesLocation, profileCollection, ".xml");
-            importToManager(ontologiesLocation, owlModel, ".obo");
-            importToManager(articlesLocation, textSourceCollection, ".txt");
-            importToManager(annotationsLocation, textSourceCollection, ".xml");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        loadProject();
-    }
-
-    /*
+	/*
     GETTERS
      */
-    public OWLModel getOWLModel() {
-        return owlModel;
-    }
 
-    public ProfileCollection getProfileCollection() {
-        return profileCollection;
-    }
+	void importProject(File profilesLocation, File ontologiesLocation, File articlesLocation, File annotationsLocation, File projectLocation) {
+		try {
+			setSaveLocation(projectLocation);
+			makeProjectStructure();
+			importToManager(profilesLocation, profileCollection, ".xml");
+			importToManager(ontologiesLocation, owlModel, ".obo");
+			importToManager(articlesLocation, textSourceCollection, ".txt");
+			importToManager(annotationsLocation, textSourceCollection, ".xml");
 
-    public SelectionModel getSelectionModel() {
-        return selectionModel;
-    }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		loadProject();
+	}
 
-    public TextSourceCollection getTextSourceCollection() {
-        return textSourceCollection;
-    }
+	/**
+	 * @return The filter model
+	 */
+	public FilterModel getFilterModel() {
+		return filterModel;
+	}
 
-    public void verifyId(String id, KnowtatorDataObjectInterface obj, Boolean hasPriority) {
-        String verifiedId = id;
-        if (hasPriority && idRegistry.keySet().contains(id)) {
-            verifyId(id, idRegistry.get(id), false);
-        } else {
-            int i = idRegistry.size();
-            while (verifiedId == null || idRegistry.keySet().contains(verifiedId)) {
-                if (obj instanceof KnowtatorTextBoundDataObjectInterface && ((KnowtatorTextBoundDataObjectInterface) obj).getTextSource() != null) {
-                    verifiedId = ((KnowtatorTextBoundDataObjectInterface) obj).getTextSource().getId() + "-" + Integer.toString(i);
-                } else {
-                    verifiedId = Integer.toString(i);
-                }
-                i++;
-            }
-        }
-        idRegistry.put(verifiedId, obj);
-        obj.setId(id == null ? verifiedId : id);
+	/**
+	 * @return A list of the models that are instances of BaseKnowtatorManager
+	 */
+	@Override
+	List<BaseKnowtatorManager> getManagers() {
+		return models.stream().filter(model -> model instanceof BaseKnowtatorManager).map(model -> (BaseKnowtatorManager) model).collect(Collectors.toList());
+	}
 
-    }
+	/**
+	 * Saves the project. Overriden here because the OWL model needs to be saved as well.
+	 *
+	 * @param ioUtilClass The IOUtil to use to save the IO class. This specifies the output format
+	 * @param basicIO     The IO class to save
+	 * @param file        The file to save to
+	 * @param <I>         The IO class
+	 * @see edu.ucdenver.ccp.knowtator.ProjectManager
+	 */
+	@Override
+	public <I extends BasicIO> void saveToFormat(Class<? extends BasicIOUtil<I>> ioUtilClass, I basicIO, File file) {
+		if (isNotLoading()) {
+			owlModel.save();
+		}
+		super.saveToFormat(ioUtilClass, basicIO, file);
+	}
 
-    public void registerAction(AbstractKnowtatorAction action) {
-        if (action != null) {
-            try {
-                action.execute();
-                addEdit(action.getEdit());
-            } catch (ActionUnperformableException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	/**
+	 * @return The OWL model
+	 */
+	public OWLModel getOWLModel() {
+		return owlModel;
+	}
 
-    @Override
-    public void dispose() {
-        models.forEach(KnowtatorObjectInterface::dispose);
-        idRegistry.clear();
-    }
+	/**
+	 * @return The profile collection
+	 */
+	public ProfileCollection getProfileCollection() {
+		return profileCollection;
+	}
 
-    public static void main(String[] args) {
-        log.warn("Knowtator");
-    }
+	/*
+	VERIFICATION
+	 */
 
-    public void reset(OWLWorkspace owlWorkspace) {
-        owlModel.setOwlWorkSpace(owlWorkspace);
-        models.forEach(BaseKnowtatorModel::reset);
-    }
+	/**
+	 * @return The selection model
+	 */
+	public SelectionModel getSelectionModel() {
+		return selectionModel;
+	}
 
-    @Override
-    public <I extends BasicIO> void saveToFormat(Class<? extends BasicIOUtil<I>> ioUtilClass, I basicIO, File file) {
-        if (isNotLoading()){
-            owlModel.save();
-        }
-        super.saveToFormat(ioUtilClass, basicIO, file);
-    }
+	/**
+	 * @return The text source collection
+	 */
+	public TextSourceCollection getTextSourceCollection() {
+		return textSourceCollection;
+	}
 
-    @Override
-    public void save() {
+	/**
+	 * This method ensures that all objects in the model will have a unique ID. If an object if provided with priority,
+	 * its id will be kept and any other object already verified will have its ID changed. IDs are changed to the form
+	 * textSourceID-int where textSourceID is the ID of the object's encompassing text source and int is the next
+	 * number not used in any other IDs in the same encompassing text source.
+	 *
+	 * @param id          The proposed id for the object
+	 * @param obj         The knowtator object
+	 * @param hasPriority True if the object should have priority over preexisting objects
+	 */
+	public void verifyId(String id, KnowtatorDataObjectInterface obj, Boolean hasPriority) {
+		String verifiedId = id;
 
-    }
+		if (hasPriority && idRegistry.keySet().contains(id)) {
+			verifyId(id, idRegistry.get(id), false);
+		} else {
+			int i = idRegistry.size();
 
-    @Override
-    public void load() {
+			while (verifiedId == null || idRegistry.keySet().contains(verifiedId)) {
+				if (obj instanceof KnowtatorTextBoundDataObjectInterface && ((KnowtatorTextBoundDataObjectInterface) obj).getTextSource() != null) {
+					verifiedId = ((KnowtatorTextBoundDataObjectInterface) obj).getTextSource().getId() + "-" + Integer.toString(i);
+				} else {
+					verifiedId = Integer.toString(i);
+				}
+				i++;
+			}
+		}
+		idRegistry.put(verifiedId, obj);
+		obj.setId(id == null ? verifiedId : id);
 
-    }
+	}
+
+	/**
+	 * Registers an action and adds its edit to the undo manager
+	 *
+	 * @param action An executable and undoable action
+	 */
+	public void registerAction(AbstractKnowtatorAction action) {
+		if (action != null) {
+			try {
+				action.execute();
+				addEdit(action.getEdit());
+			} catch (ActionUnperformableException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Disposes the model. Clears the id registry.
+	 */
+	@Override
+	public void dispose() {
+		models.forEach(KnowtatorObjectInterface::dispose);
+		idRegistry.clear();
+	}
+
+	/**
+	 * Resets the model to its initial condition.
+	 *
+	 * @param owlWorkspace Protege's OWL workspace to be passed to the OWL model.
+	 */
+	public void reset(OWLWorkspace owlWorkspace) {
+		owlModel.setOwlWorkSpace(owlWorkspace);
+		models.forEach(BaseKnowtatorModel::reset);
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public void save() {
+
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public void load() {
+
+	}
 }
