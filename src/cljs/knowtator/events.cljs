d8c853d680fb3b02ec15912c6275e25fe667b629
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-11 19:21:39 -0700

    Re-added the annotation-panel events.

--- src/cljs/knowtator/events.cljs
+++ src/cljs/knowtator/events.cljs
@@ -1,6 +1,12 @@
 (ns knowtator.events
   (:require [day8.re-frame.tracing :refer-macros [fn-traced]]
+            [ajax.core :as ajax]
+            [day8.re-frame.http-fx]
+            [clojure.string :as str]
+            [day8.re-frame.tracing :refer-macros [fn-traced]]
+            [day8.re-frame.undo :as undo :refer [undoable]]
             [knowtator.db :as db]
+            [knowtator.model :as model]
             [re-frame.core :as re-frame]
             [re-pressed.core :as rp]))
 
@@ -41,3 +47,179 @@
     (update-in db [:graph :nodes] conj {:id (count (lazy-cat
                                                      (get-in db [:graph :nodes])
                                                      (get-in db [:graph :edges])))})))
+
+
+(re-frame/reg-event-fx
+  ::import-owl
+  (fn [state _]
+    (assoc state :http-xhiro {:method          :get
+                              :uri             "/ontology"
+                              :timeout         3000
+                              :response-format (ajax/transit-response-format)
+                              :on-success      [:add-ontology]
+                              :on-failure      [:handle-failure]})))
+
+(re-frame/reg-event-db
+  ::select-span
+  (fn [db [_ loc doc-id]]
+    (let [anns             (:anns db)
+          {:keys [id ann]} (->> db
+                             :spans
+                             (model/filter-in-restriction {:doc doc-id} anns)
+                             (model/spans-containing-loc loc)
+                             vals
+                             first)]
+      (-> db
+        (assoc-in [:selection :span] id)
+        (assoc-in [:selection :ann] ann)))))
+
+(re-frame/reg-event-fx
+  ::record-selection
+  (fn [{:keys [db]} [_ {:keys [start end] :as text-range} doc-id]]
+    (cond-> {:db (update db :selection merge text-range)}
+      (= start end) (assoc :dispatch [::select-span start doc-id]))))
+
+(re-frame/reg-event-db
+  ::select-profile
+  (fn [db [_ profile-id]]
+    (assoc-in db [:selection :profile] profile-id)))
+
+(re-frame/reg-event-db
+  ::toggle-profile-restriction
+  (fn [db]
+    (update-in db [:selection :profile-restriction] not)))
+
+(re-frame/reg-event-db
+  ::select-doc
+  (fn [db [_ doc-id]]
+    (assoc-in db [:selection :doc] doc-id)))
+
+
+(re-frame/reg-event-db
+  ::add-doc
+  (undoable "Adding document")
+  (fn [db [_]]
+    (let [doc-id (model/unique-id db :docs "d" (-> db :docs count))]
+      (-> db
+        (assoc-in [:docs doc-id]  {:id      doc-id
+                                   :content (str "I'm called " doc-id)})
+        (assoc-in [:selection :doc] doc-id)))))
+
+(re-frame/reg-event-db
+  ::add-ann
+  (undoable "Adding annotation")
+  (fn [db [_]]
+    (let [span-id                                 (model/unique-id db :spans "s" (-> db :spans count))
+          ann-id                                  (model/unique-id db :anns "a" (-> db :anns count))
+          {:keys [profile concept doc end start]} (:selection db)]
+      (-> db
+        (assoc-in [:anns ann-id]  {:id      ann-id
+                                   :profile profile
+                                   :concept concept
+                                   :doc     doc})
+        (assoc-in [:spans span-id] {:id    span-id
+                                    :ann   ann-id
+                                    :end   end
+                                    :start start})
+        (assoc-in [:selection :ann] ann-id)
+        (assoc-in [:selection :span] span-id)))))
+
+(re-frame/reg-event-db
+  ::remove-selected-doc
+  (undoable "Removing document")
+  (fn [db [_]]
+    (-> db
+      (update :docs dissoc (get-in db [:selection :doc]))
+      (assoc-in [:selection :doc] (-> db
+                                    :docs
+                                    keys
+                                    first)))))
+(re-frame/reg-event-db
+  ::remove-selected-ann
+  (undoable "Removing annotation")
+  (fn [db [_]]
+    (-> db
+      (update :anns dissoc (get-in db [:selection :ann]))
+      (update :spans dissoc (get-in db [:selection :span]))
+      (assoc-in [:selection :ann] nil)
+      (assoc-in [:selection :span] nil))))
+
+
+(re-frame/reg-event-db
+  ::select-prev-doc
+  #(model/cycle-selection % :doc :docs :prev))
+
+(re-frame/reg-event-db
+  ::select-next-doc
+  #(model/cycle-selection % :doc :docs :next))
+
+(re-frame/reg-event-db
+  ::select-prev-span
+  #(model/cycle-selection % :span :spans :prev))
+
+(re-frame/reg-event-db
+  ::select-next-span
+  #(model/cycle-selection % :span :spans :next))
+
+(re-frame/reg-event-db
+  ::grow-selected-span-start
+  (undoable "Growing span start")
+  #(model/mod-span % :start dec))
+
+(re-frame/reg-event-db
+  ::shrink-selected-span-start
+  (undoable "Shrinking span start")
+  #(model/mod-span % :start inc))
+
+(re-frame/reg-event-db
+  ::shrink-selected-span-end
+  (undoable "Shrinking span end")
+  #(model/mod-span % :end dec))
+
+(re-frame/reg-event-db
+  ::grow-selected-span-end
+  (undoable "Growing span end")
+  #(model/mod-span % :end inc))
+
+(re-frame/reg-event-db
+  ::find-in-selected-doc
+  (fn [db]
+    (let [doc-id            (get-in db [:selection :doc])
+          doc               (get-in db [:docs doc-id :content])
+          text              (get-in db [:search :query])
+          last-search-start (get-in db [:spans :last-search-span :start])
+          result            (or
+                              (str/index-of doc text (inc last-search-start))
+                              (str/index-of doc text))]
+      ;; TODO the last-search-span, when overlapped with selected span, causes division on overlapped span.
+      (-> db
+        (assoc-in [:spans :last-search-span] {:id       :last-search-span
+                                              :searched result
+                                              :ann      :last-search-ann
+                                              :start    result
+                                              :end      (when result (+ result (count text)))})
+        (assoc-in [:anns :last-search-ann] {:id  :last-search-ann
+                                            :doc doc-id})
+        (assoc-in [:search :un-searched?] true)))))
+
+(re-frame/reg-event-fx
+  ::update-search-text
+  (fn [{:keys [db]} [_ text]]
+    {:db       (assoc-in db [:search :query] text)
+     :dispatch [::find-in-selected-doc text]}))
+
+(re-frame/reg-event-db
+  ::done-searching
+  (fn [db]
+    (assoc-in db [:search :un-searched?] false)))
+
+
+(re-frame/reg-event-db
+  ::set-concept-color
+  (undoable "Setting color for concept")
+  (fn [db [_ color]]
+    (assoc-in db [:profiles
+                  (get-in db [:selection :profile])
+                  :colors
+                  (get-in db [:selection :concept])]
+      color)))
