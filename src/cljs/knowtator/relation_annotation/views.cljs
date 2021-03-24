b1d1a8171b016c4ccb8219a8ea1e6ddccce699b6
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-24 12:29:03 -0600

    Made graph-view easier to work with (handler-fn, etc.).

--- src/cljs/knowtator/relation_annotation/views.cljs
+++ src/cljs/knowtator/relation_annotation/views.cljs
@@ -2,30 +2,32 @@
   (:require ["react-graph-vis" :as rgv]
             [knowtator.relation-annotation.events :as evts]
             [knowtator.relation-annotation.subs :as subs]
-            [knowtator.util :refer [<sub >evt]]
+            [knowtator.util :as util :refer [<sub >evt]]
             [re-frame.core :as rf]
             [reagent.core :as r]))
 
+(def visjs-graph (aget rgv "default"))
+
+(defn handler-fn
+  [f]
+  (fn [e]
+    (f (js->clj e :keywordize-keys true))))
+
 (defn vis [graph & {:keys [options events style]
                     :or {style {:height "640px"}}}]
-  [(r/adapt-react-class (aget rgv "default"))
+  [(r/adapt-react-class visjs-graph)
    {:graph   graph
     :options options
-    :events  events
+    :events  (util/map-vals handler-fn events)
     :style   style}])
 
 (defn graph []
   [vis (<sub [::subs/graph])
    :options {:layout {:hierarchical false}
              :edges  {:color "#000000"}}
-   :events  {:select       (fn [e]
-                             (let [e                     (js->clj e :keywordize-keys true)
-                                   {:keys [nodes edges]} e]
-                               (println "Nodes:" nodes)
-                               (println "Edges:" edges)))
-             :double-click (fn [e]
-                             (let [{:keys [x y]} (-> e
-                                                   (js->clj :keywordize-keys true)
-                                                   (get-in [:pointer :canvas]))]
-                               (println x y)
-                               (rf/dispatch [::evts/add-node])))}])
+   :events  {:select       (fn [{:keys [nodes edges]}]
+                             (println "Nodes:" nodes)
+                             (println "Edges:" edges))
+             :double-click (fn [{{{:keys [x y]} :canvas} :pointer}]
+                             (println x y)
+                             (rf/dispatch [::evts/add-node]))}])
