7587fc23567e04543a9c484545a5156c5d56a0d4
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-24 23:57:25 -0600

    Removed now unused server file.

--- src/clj/knowtator/server.clj
+++
@@ -1,9 +0,0 @@
-(ns my-app.server
-  (:require [my-app.handler :refer [handler]]
-            [config.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
-  (:gen-class))
-
- (defn -main [& _args]
-   (let [port (or (env :port) 3000)]
-     (run-jetty handler {:port port :join? false})))
