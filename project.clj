f20fa928920867e5ca31dafe7d815df17b6fc12e
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-24 20:02:50 -0600

    Removed project.clj elements not needed for lein-garden.

--- project.clj
+++ project.clj
@@ -1,39 +1,10 @@
 (defproject knowtator "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/clojurescript "1.10.773"
-                  :exclusions [com.google.javascript/closure-compiler-unshaded
-                               org.clojure/google-closure-library
-                               org.clojure/google-closure-library-third-party]]
-                 [thheller/shadow-cljs "2.11.21"]
-                 [reagent "1.0.0"]
-                 [re-frame "1.2.0"]
-                 [day8.re-frame/tracing "0.6.0"]
-                 [re-com "2.13.2"]
-                 [bidi "2.1.6"]
-                 [kibu/pushy "0.3.8"]
-                 [garden "1.3.10"]
-                 [ns-tracker "0.4.0"]
-                 [re-pressed "0.3.1"]
-                 [breaking-point "0.1.2"]]
+                 [garden "1.3.10"]]
 
-  :plugins [[cider/cider-nrepl "0.25.6"]
-            [lein-shadow "0.3.1"]
-            [lein-garden "0.3.0"]
-            [lein-shell "0.5.0"]]
-
+  :plugins [[lein-garden "0.3.0"]]
   :min-lein-version "2.9.0"
-
-  :jvm-opts ["-Xmx1G"]
-
-  :source-paths ["src/clj" "src/cljs"]
-
-  :test-paths   ["test/cljs"]
-
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"
-                                    "test/js"
-                                    "resources/public/css"]
-
-
+  :source-paths ["src/clj"]
   :garden {:builds [{:id           "screen"
                      :source-paths ["src/clj"]
                      :stylesheet   knowtator.css/screen
@@ -43,77 +14,4 @@
                      :source-paths ["src/clj"]
                      :stylesheet   knowtator.css/datatable
                      :compiler     {:output-to     "resources/public/css/datatable.css"
-                                    :pretty-print? true}}]}
-
-  :shadow-cljs {:nrepl {:port 8777}
-
-                :builds {:app {:target     :browser
-                               :output-dir "resources/public/js/compiled"
-                               :asset-path "/js/compiled"
-                               :modules    {:app {:init-fn  knowtator.core/init
-                                                  :preloads [devtools.preload
-                                                             day8.re-frame-10x.preload]}}
-                               :dev        {:compiler-options {:closure-defines {re-frame.trace.trace-enabled?              true
-                                                                                 day8.re-frame.tracing.trace-enabled?       true
-                                                                                 re-com.config/root-url-for-compiler-output "http://localhost:8290/js/compiled/app/cljs-runtime/"}}}
-                               :release    {:build-options
-                                            {:ns-aliases
-                                             {day8.re-frame.tracing day8.re-frame.tracing-stubs}}}
-
-                               :devtools {:http-root "resources/public"
-                                          :http-port 8280
-                                          }}
-                         :browser-test
-                         {:target    :browser-test
-                          :ns-regexp "-test$"
-                          :runner-ns shadow.test.browser
-                          :test-dir  "target/browser-test"
-                          :devtools  {:http-root "target/browser-test"
-                                      :http-port 8290}}
-
-                         :karma-test
-                         {:target    :karma
-                          :ns-regexp "-test$"
-                          :output-to "target/karma-test.js"}}}
-
-  :shell {:commands {"karma" {:windows         ["cmd" "/c" "karma"]
-                              :default-command "karma"}
-                     "open"  {:windows ["cmd" "/c" "start"]
-                              :macosx  "open"
-                              :linux   "xdg-open"}}}
-
-  :aliases {"dev"   ["do"
-                     ["shell" "echo" "\"DEPRECATED: Please use lein watch instead.\""]
-                     ["watch"]]
-            "watch" ["with-profile" "dev" "do"
-                     ["shadow" "watch" "app" "browser-test" "karma-test"]]
-
-            "prod" ["do"
-                    ["shell" "echo" "\"DEPRECATED: Please use lein release instead.\""]
-                    ["release"]]
-
-            "release" ["with-profile" "prod" "do"
-                       ["shadow" "release" "app"]]
-
-            "build-report" ["with-profile" "prod" "do"
-                            ["shadow" "run" "shadow.cljs.build-report" "app" "target/build-report.html"]
-                            ["shell" "open" "target/build-report.html"]]
-
-            "karma" ["do"
-                     ["shell" "echo" "\"DEPRECATED: Please use lein ci instead.\""]
-                     ["ci"]]
-            "ci"    ["with-profile" "prod" "do"
-                     ["shadow" "compile" "karma-test"]
-                     ["shell" "karma" "start" "--single-run" "--reporters" "junit,dots"]]}
-
-  :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "1.0.2"]
-                   [day8.re-frame/re-frame-10x "1.0.1"]]
-    :source-paths ["dev"]}
-
-   :prod {}
-
-   }
-
-  :prep-tasks [["garden" "once"]])
+                                    :pretty-print? true}}]})
