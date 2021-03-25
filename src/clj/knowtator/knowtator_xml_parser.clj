5a3aa91451ed84d6d60516fc89c79e7e6630b448
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-25 16:25:11 -0600

    Moved other individual items to own parsing fns.

--- src/clj/knowtator/knowtator_xml_parser.clj
+++ src/clj/knowtator/knowtator_xml_parser.clj
@@ -19,67 +19,45 @@
         (map xml/parse)
         (map (partial walk/postwalk struct->map))))))
 
-(defn parse-documents [project-file xmls]
-  (letfn [(file-name [f]
-            (str (.getName f)))
-          (file-name->id [f]
-            (-> f
-              file-name
-              (str/replace-first #"\.txt$" "")))]
-    (let [articles (-> project-file
-                     (io/file "Articles")
-                     file-seq
-                     rest
-                     (->>
-                       (map (juxt file-name file-name->id slurp))
-                       (map (partial zipmap [:file-name :id :content]))
-                       (util/map-with-key :id)))]
-      (->> xmls
-        (mapcat #(m/rewrites %
-                   {:tag     :knowtator-project
-                    :content (m/scan {:tag   :document
-                                      :attrs {:id        ?doc
-                                              :text-file ?file-name}})}
-                   {:id        ?doc
-                    :file-name ?file-name}))
-        (util/map-with-key :id)
-        (merge-with (partial merge-with (comp (partial some identity) vector)) articles)
-        vals))))
+(defn parse-profile [xml]
+  (m/rewrites xml
+    {:tag     :knowtator-project
+     :content (m/scan
+                {:tag     :profile
+                 :attrs   {:id ?id}
+                 :content [{:tag   :highlighter
+                            :attrs {:class !concept
+                                    :color !color}}
+                           ...]})}
+    {:id     (m/app keyword ?id)
+     :colors (m/app (partial apply hash-map) [!concept !color ...])}))
 
-(defn parse-annotations [xmls]
-  (->> xmls
-    (mapcat #(m/rewrites %
-               {:tag     :knowtator-project
-                :content (m/scan {:tag     :document
-                                  :attrs   {:id ?doc}
-                                  :content (m/scan
-                                             {:tag     :annotation
-                                              :attrs   {:id        ?ann
-                                                        :annotator ?profile}
-                                              :content (m/scan
-                                                         {:tag   :class
-                                                          :attrs {:id    ?concept
-                                                                  :label ?concept-label}
-                                                          :as    ?other})})})}
-               {:id      ?ann
-                :doc     ?doc
-                :profile ?profile
-                :concept ?concept-label}))
-    set))
+(defn parse-document [xml]
+  (m/rewrites xml
+    {:tag     :knowtator-project
+     :content (m/scan {:tag   :document
+                       :attrs {:id        ?doc
+                               :text-file ?file-name}})}
+    {:id        ?doc
+     :file-name ?file-name}))
 
-(defn parse-profiles [xmls]
-  (->> xmls
-    (mapcat #(m/rewrites %
-               {:tag     :knowtator-project
-                :content (m/scan
-                           {:tag     :profile
-                            :attrs   {:id ?id}
-                            :content [{:tag   :highlighter
-                                       :attrs {:class !concept
-                                               :color !color}}
-                                      ...]})}
-               {:id     (m/app keyword ?id)
-                :colors (m/app (partial apply hash-map) [!concept !color ...])}))))
+(defn parse-annotation [xml]
+  (m/rewrites xml
+    {:tag     :knowtator-project
+     :content (m/scan {:tag     :document
+                       :attrs   {:id ?doc}
+                       :content (m/scan
+                                  {:tag     :annotation
+                                   :attrs   {:id        ?ann
+                                             :annotator ?profile}
+                                   :content (m/scan
+                                              {:tag   :class
+                                               :attrs {:id    ?concept
+                                                       :label ?concept-label}})})})}
+    {:id      ?ann
+     :doc     ?doc
+     :profile ?profile
+     :concept ?concept-label}))
 
 (defn parse-span [verify-id xml]
   (m/rewrites xml
@@ -99,6 +77,38 @@
      :ann   ?ann
      :start (m/app #(Integer/parseInt %) ?start)
      :end   (m/app #(Integer/parseInt %) ?end)}))
+
+(defn parse-documents [project-file xmls]
+  (letfn [(file-name [f]
+            (str (.getName f)))
+          (file-name->id [f]
+            (-> f
+              file-name
+              (str/replace-first #"\.txt$" "")))]
+    (let [articles (-> project-file
+                     (io/file "Articles")
+                     file-seq
+                     rest
+                     (->>
+                       (map (juxt file-name file-name->id slurp))
+                       (map (partial zipmap [:file-name :id :content]))
+                       (util/map-with-key :id)))]
+      (->> xmls
+        (mapcat parse-document)
+        (util/map-with-key :id)
+        (merge-with (partial merge-with (comp (partial some identity) vector)) articles)
+        vals))))
+
+
+(defn parse-annotations [xmls]
+  (->> xmls
+    (mapcat parse-annotation)
+    set))
+
+(defn parse-profiles [xmls]
+  (->> xmls
+    (mapcat parse-profile)))
+
 
 (defn parse-spans [xmls]
   (let [id-idx (atom 0)]
