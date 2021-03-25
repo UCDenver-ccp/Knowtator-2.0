159915965ab134f7ba458041291de9cbc1790b92
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-25 14:05:41 -0600

    Parse concept annotations from annotation file.

--- src/clj/knowtator/knowtator_xml_parser.clj
+++ src/clj/knowtator/knowtator_xml_parser.clj
@@ -1,2 +1,43 @@
 (ns knowtator.knowtator-xml-parser
-  (:require [clojure.core.match :as match]))
+  (:require [clojure.java.io :as io]
+            [clojure.walk :as walk]
+            [clojure.xml :as xml]
+            [meander.epsilon :as m]))
+
+(defn read-annotation-files [project-file-name]
+  (letfn [(struct->map [x]
+            (cond->> x
+              (instance? clojure.lang.PersistentStructMap x)
+              (into {})))]
+    (-> project-file-name
+      (io/file "Annotations")
+      str
+      io/resource
+      io/file
+      file-seq
+      rest
+      (->>
+        (map xml/parse)
+        (map (partial walk/postwalk struct->map))))))
+
+(defn parse-annotations [file-name]
+  (->> file-name
+    read-annotation-files
+    (mapcat #(m/rewrites %
+               {:tag     :knowtator-project
+                :attrs   nil
+                :content (m/scan {:tag     :document
+                                  :attrs   {:id ?doc}
+                                  :content (m/scan
+                                             {:tag     :annotation
+                                              :attrs   {:id        ?ann
+                                                        :annotator ?profile}
+                                              :content (m/scan
+                                                         {:tag   :class
+                                                          :attrs {:id    ?concept
+                                                                  :label ?concept-label}
+                                                          :as    ?other})})})}
+               {:id      ?ann
+                :doc     ?doc
+                :profile ?profile
+                :concept ?concept-label}))))
--- test/clj/knowtator/knowtator_xml_parser_test.clj
+++ test/clj/knowtator/knowtator_xml_parser_test.clj
@@ -1,6 +1,63 @@
 (ns knowtator.knowtator-xml-parser-test
-  (:require [knowtator.knowtator-xml-parser :as sut]
-            [clojure.test :refer [testing deftest is]]))
+  (:require [clojure.test :refer [deftest is testing]]
+            [knowtator.knowtator-xml-parser :as sut]))
 
-(deftest parse-annotation-file-test
-  (testing "Basic parse annotation file"))
+(deftest parse-annotations-test
+  (testing "Basic parse annotation file"
+    (is (= #{{:id      "mention_0",
+              :doc     "document1",
+              :profile "Default",
+              :concept "Pizza"}
+             {:id      "mention_1",
+              :doc     "document1",
+              :profile "profile1",
+              :concept "IceCream"}
+             {:id      "mention_3",
+              :doc     "document2",
+              :profile "Default",
+              :concept nil}
+             {:id      "mention_0",
+              :doc     "document3",
+              :profile "Default",
+              :concept "Food"}
+             {:id      "mention_1",
+              :doc     "document3",
+              :profile "profile1",
+              :concept "Food"}
+             {:id      "mention_2",
+              :doc     "document3",
+              :profile "Default",
+              :concept "Food"}}
+          (-> "test_project_using_uris"
+            sut/parse-annotations
+            set)))))
+
+;; public static final ProjectCounts defaultCounts = new ProjectCounts(5, 6, 7, 3, 2, 3, 7, 4, 0);
+
+;; /** The constant defaultExpectedTextSources. */
+;; public static final int defaultExpectedTextSources = 5;
+
+;; /** The constant defaultExpectedConceptAnnotations. */
+;; public static final int defaultExpectedConceptAnnotations = 6;
+
+;; public static final int defaultExpectedStructureAnnotations = 0;
+
+;; /** The constant defaultExpectedSpans. */
+;; public static final int defaultExpectedSpans = 7;
+
+;; /** The constant defaultExpectedGraphSpaces. */
+;; public static final int defaultExpectedGraphSpaces = 3;
+
+;; /** The constant defaultExpectedProfiles. */
+;; public static final int defaultExpectedProfiles = 2;
+
+;; /** The constant defaultExpectedHighlighters. */
+;; public static final int defaultExpectedHighlighters = 3;
+
+;; /** The constant defaultExpectedAnnotationNodes. */
+;; public static final int defaultExpectedAnnotationNodes = 7;
+
+;; /** The constant defaultExpectedTriples. */
+;; public static final int defaultExpectedTriples = 4;
+
+;; public static int defaultAnnotationLayers = 1;
