d11058bcd7ec181a1431b52487cf4dc39d94a3c9
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-03-24 19:07:37 -0600

    Added test for realize-ann.

--- test/clj/knowtator/model_test.clj
+++ test/clj/knowtator/model_test.clj
@@ -1,34 +1,54 @@
 (ns knowtator.model-test
-  (:require [knowtator.model.util :as model]
-            [clojure.test :as t]
-            [knowtator.util :as util]))
+  (:require [clojure.test :refer [testing is deftest]]
+            [knowtator.model :as sut]))
 
-(t/deftest ann-color-test
-  (t/testing "Annotation color from profile"
-    (t/is (= "blue"
-            (model/ann-color
-              {:id      :a1
-               :profile :p1
-               :concept :c1
-               :doc     :d1}
-              {:p1 {:c1 "blue"
-                    :c2 "green"}})))))
+(deftest realize-ann-test
+  (testing "Realize simple ann"
+    (is (= {:id      :a1
+            :profile :p1
+            :concept :c1
+            :doc     :d1
+            :color   "green"
+            :content "The brown fox jumped over the lazy dog."}
+          (sut/realize-ann
+            {:p1 {:c1 "green"
+                  :c2 "red"}
+             :p2 {:c1 "blue"}}
+            [{:id      :s1
+              :ann     :a1
+              :start   1
+              :end     10
+              :content "The brown fox"}
+             {:id      :s2
+              :ann     :a2
+              :start   2
+              :end     11
+              :content "WRONG"}
+             {:id      :s3
+              :ann     :a1
+              :start   20
+              :end     24
+              :content "jumped over the lazy dog."}]
+            {:id      :a1
+             :profile :p1
+             :concept :c1
+             :doc     :d1})))))
 
-(t/deftest make-overlapping-spans-test
-  (t/testing "Empty input"
-    (t/is (empty? (model/make-overlapping-spans []))))
-  (t/testing "Exact overlap"
-    (t/is (= 1 (-> '({:id :Z+O/+!e+_, :ann :H!3_?cS?/-, :start 79, :end 92}
-                     {:id #{:Z+O/+!e+_ :Ff/aa?W}, :ann #{:H!3_?cS?/- :Q+d/K*+?EY}, :start 79, :end 92})
-                 model/make-overlapping-spans
-                 count)))
-    (t/is (= 1 (-> '({:id    #{:LB0/_**Y956. :F?i!XJ/Ff :.-5!+o_/l-.E.},
-                      :ann   #{:BBL:E9/muM :jz.zzF/DIDi :La9b_/R},
-                      :start 30,
-                      :end   36}
-                     {:id    #{:_XE/x :LB0/_**Y956. :F?i!XJ/Ff :.-5!+o_/l-.E. :Wr./R?GQ-},
-                      :ann   #{:BBL:E9/muM :jz.zzF/DIDi :++oLJj0/TC! :La9b_/R :y?/Ss8d4.},
-                      :start 30,
-                      :end   36})
-                 model/make-overlapping-spans
-                 count)))))
+(deftest make-overlapping-spans-test
+  (testing "Empty input"
+    (is (empty? (sut/make-overlapping-spans []))))
+  (testing "Exact overlap"
+    (is (= 1 (-> '({:id :Z+O/+!e+_, :ann :H!3_?cS?/-, :start 79, :end 92}
+                   {:id #{:Z+O/+!e+_ :Ff/aa?W}, :ann #{:H!3_?cS?/- :Q+d/K*+?EY}, :start 79, :end 92})
+               sut/make-overlapping-spans
+               count)))
+    (is (= 1 (-> '({:id    #{:LB0/_**Y956. :F?i!XJ/Ff :.-5!+o_/l-.E.},
+                    :ann   #{:BBL:E9/muM :jz.zzF/DIDi :La9b_/R},
+                    :start 30,
+                    :end   36}
+                   {:id    #{:_XE/x :LB0/_**Y956. :F?i!XJ/Ff :.-5!+o_/l-.E. :Wr./R?GQ-},
+                    :ann   #{:BBL:E9/muM :jz.zzF/DIDi :++oLJj0/TC! :La9b_/R :y?/Ss8d4.},
+                    :start 30,
+                    :end   36})
+               sut/make-overlapping-spans
+               count)))))
