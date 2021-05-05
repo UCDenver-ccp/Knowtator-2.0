06a630211c2c76bb8ecfb5af39746da4b80ab36d
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-05-05 13:04:08 -0600

    Fixed sizing and resizing of analogy graphs.

--- src/cljs/knowtator/relation_annotation/views.cljs
+++ src/cljs/knowtator/relation_annotation/views.cljs
@@ -18,7 +18,7 @@
   [id graph
    {:keys [options events style]
     {:keys [width height]
-     :or   {height "50vh"
+     :or   {height "100%"
             width  "100%"}}
     :style}]
   (when-let [graph (<sub graph)]
@@ -27,43 +27,50 @@
       :options (-> options
                    (update :manipulation
                            (partial util/map-vals manipulation-handler-fn))
-                   (assoc-in [:manipulation :enabled] true))
+                   (assoc-in [:manipulation :enabled] true)
+                   (assoc :auto-resize true))
       :events  (util/map-vals handler-fn events)
       :style   (-> style
-                   (assoc :height height :width width))}]))
+                   (assoc :height height :width width :position :absolute))}]))
 
 (defn graph
   [graph-id graph-sub graph-id-sub & [options]]
-  [vis graph-id graph-sub
-   (merge-with
-    merge
-    {:options {:layout       {:hierarchical false}
-               :edges        {:color "#000000"}
-               :physics      {:enabled    (<sub [::subs/graph-physics])
-                              :barnes-hut {:spring-length
-                                           (<sub [::subs/edge-length])}}
-               :interaction  {:hover true}
-               :manipulation {:add-node    (fn [node-data]
-                                             (>evt [::evts/add-node
-                                                    (<sub graph-id-sub)
-                                                    node-data]))
-                              :add-edge    (fn [edge-data]
-                                             (>evt [::evts/add-edge
-                                                    (<sub graph-id-sub)
-                                                    edge-data]))
-                              :edit-node   (fn [node-data] (println node-data))
-                              :edit-edge   (fn [edge-data] (println edge-data))
-                              :delete-node (fn [node-ids] (println node-ids))
-                              :delete-edge (fn [edge-ids] (println edge-ids))}}
-     :events  {:click       (fn [{:keys                   [nodes]
-                                  {{:keys [x y]} :canvas} :pointer}]
-                              (>evt [::evts/toggle-node-physics
-                                     (<sub graph-id-sub) (first nodes) x y]))
-               :select-node (fn [{:keys [nodes]}]
-                              (>evt [::evts/select-ann-node (<sub graph-id-sub)
-                                     (first nodes)]))
-               :select-edge (fn [{:keys [edges]}]
-                              (>evt [::evts/select-relation-ann
-                                     (<sub graph-id-sub) (first edges)]))}
-     :style   {:background-color "white"}}
-    options)])
+  [:div {:style {:position :relative
+                 :height   "100%"
+                 :width    "100%"}}
+    [vis graph-id graph-sub
+     (merge-with
+      merge
+      {:options {:layout       {:hierarchical false}
+                 :edges        {:color "#000000"}
+                 :physics      {:enabled    (<sub [::subs/graph-physics])
+                                :barnes-hut {:spring-length
+                                             (<sub [::subs/edge-length])}}
+                 :interaction  {:hover true}
+                 :manipulation {:add-node    (fn [node-data]
+                                               (>evt [::evts/add-node
+                                                      (<sub graph-id-sub)
+                                                      node-data]))
+                                :add-edge    (fn [edge-data]
+                                               (>evt [::evts/add-edge
+                                                      (<sub graph-id-sub)
+                                                      edge-data]))
+                                :edit-node   (fn [node-data]
+                                               (println node-data))
+                                :edit-edge   (fn [edge-data]
+                                               (println edge-data))
+                                :delete-node (fn [node-ids] (println node-ids))
+                                :delete-edge (fn [edge-ids]
+                                               (println edge-ids))}}
+       :events  {:click       (fn [{:keys                   [nodes]
+                                    {{:keys [x y]} :canvas} :pointer}]
+                                (>evt [::evts/toggle-node-physics
+                                       (<sub graph-id-sub) (first nodes) x y]))
+                 :select-node (fn [{:keys [nodes]}]
+                                (>evt [::evts/select-ann-node
+                                       (<sub graph-id-sub) (first nodes)]))
+                 :select-edge (fn [{:keys [edges]}]
+                                (>evt [::evts/select-relation-ann
+                                       (<sub graph-id-sub) (first edges)]))}
+       :style   {:background-color "white"}}
+      options)]])
