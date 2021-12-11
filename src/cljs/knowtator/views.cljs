e4b276d7d59e268a3d809feba882371a04a7977b
Harrison Pielke-Lombardo <harrison.pielke-lombardo@cuanschutz.edu>
2021-12-10 19:22:58 -0700

    Updates.

--- src/cljs/knowtator/views.cljs
+++ src/cljs/knowtator/views.cljs
@@ -19,72 +19,68 @@
             [knowtator.analogy.views :as ana-views]))
 
 
+(defn project-controls
+  []
+  [re-com/h-box
+    :children
+    [[re-com/single-dropdown
+       :src       (at)
+       :choices   (<sub [::subs/available-projects])
+       :label-fn  identity
+       :id-fn     identity
+       :model     (<sub [::subs/selected-project])
+       :on-change #(>evt [::evts/select-project %])]
+     [re-com/button
+       :src      (at)
+       :label    "Open project"
+       :on-click #(>evt [::evts/load-project (<sub [::subs/selected-project])])]
+     [re-com/alert-list
+       :on-close #(println %)
+       :alerts
+       (->>
+        [{:id         :project-loading
+          :alert-type :info
+          :heading    "Project Status"
+          :body       (if (<sub [::subs/loading? :project]) "spinny" "done")}
+         (when (<sub [::subs/error? :project])
+           {:id         :project-error
+            :alert-type :danger
+            :heading    "Project load error"})
+         {:id         :ontology-loading
+          :alert-type :info
+          :heading    "Ontology status"
+          :body       (if (<sub [::subs/loading? :ontology]) "spinny" "done")}
+         (when (<sub [::subs/error? :ontology])
+           {:id         :ontology-error
+            :alert-type :danger
+            :heading    "Ontology load error"})]
+        (keep identity)
+        vec)]]])
+
 (defn page-title
   []
   [re-com/v-box
-   :children
-   [[re-com/h-box
-     :children
-     [[:img
-       (let [size "60px"]
-         {:src   "https://avatars.githubusercontent.com/u/1854424?s=200&v=4"
-          :style {:width  size
-                  :height size}})]
-      [re-com/title :label "Knowtator" :level :level1]]]
-    #_[re-com/h-box
+    :children
+    [[re-com/h-box
        :children
-       [[re-com/single-dropdown
-         :src
-         (at)
-         :choices
-         (<sub [::subs/available-projects])
-         :label-fn
-         identity
-         :id-fn
-         identity
-         :model
-         (<sub [::subs/selected-project])
-         :on-change
-         #(>evt [::evts/select-project %])]
-        [re-com/button
-         :src
-         (at)
-         :label
-         "Open project"
-         :on-click
-         #(>evt [::evts/load-project (<sub [::subs/selected-project])])]
-        [re-com/alert-list
-         :on-close
-         #(println %)
-         :alerts
-         (->>
-          [{:id         :project-loading
-            :alert-type :info
-            :heading    "Project Status"
-            :body       (if (<sub [::subs/loading? :project]) "spinny" "done")}
-           (when (<sub [::subs/error? :project])
-             {:id         :project-error
-              :alert-type :danger
-              :heading    "Project load error"})
-           {:id         :ontology-loading
-            :alert-type :info
-            :heading    "Ontology status"
-            :body       (if (<sub [::subs/loading? :ontology]) "spinny" "done")}
-           (when (<sub [::subs/error? :ontology])
-             {:id         :ontology-error
-              :alert-type :danger
-              :heading    "Ontology load error"})]
-          (keep identity)
-          vec)]]]]])
+       [[:img
+         (let [size "60px"]
+           {:src   "https://avatars.githubusercontent.com/u/1854424?s=200&v=4"
+            :style {:width  size
+                    :height size}})]
+        [re-com/title
+          :label "Knowtator"
+          :level :level1]]] [project-controls]]])
 
 (defn undo-controls
   []
   [re-com/h-box
-   :children
-   [[re-com/md-circle-icon-button :md-icon-name "zmdi-undo" :on-click
-     #(>evt [:undo])]
-    [re-com/md-circle-icon-button :md-icon-name "zmdi-redo" :on-click
-     #(>evt [:redo])]]])
+    :children [[re-com/md-circle-icon-button
+                 :md-icon-name "zmdi-undo"
+                 :on-click     #(>evt [:undo])]
+               [re-com/md-circle-icon-button
+                 :md-icon-name "zmdi-redo"
+                 :on-click     #(>evt [:redo])]]])
 
 
 (defn target-value
@@ -97,146 +93,116 @@
 (defn color-controls
   []
   [re-com/h-box
-   :children
-   [[:input#my
-     {:type      :color
-      :class     "rc-button"
-      :value     (<sub [::subs/selected-color])
-      :on-change #(>evt [::evts/set-concept-color (target-value %)])}]]])
+    :children [[:input#my {:type      :color
+                           :class     "rc-button"
+                           :value     (<sub [::subs/selected-color])
+                           :on-change #(>evt [::evts/set-concept-color
+                                              (target-value %)])}]]])
 
 
 (defn search-controls
   []
   [re-com/h-box
-   :children
-   [[re-com/input-text
-     :model
-     (<sub [::subs/search-query])
-     :on-change
-     #(>evt [::evts/update-search-text %])
-     ;; :attr {:on-key-press #(when (html/key? % :enter)
-     ;;                         (>evt
-     ;;                         [::events/find-in-selected-doc]))}
-     :change-on-blur?
-     false
-     :placeholder
-     "search document"]
-    [re-com/label :label (<sub [::subs/search-matches])]]])
+    :children [[re-com/input-text
+                 :model           (<sub [::subs/search-query])
+                 :on-change       #(>evt [::evts/update-search-text %])
+                 ;; :attr {:on-key-press #(when (html/key? % :enter)
+                 ;;                         (>evt
+                 ;;                         [::events/find-in-selected-doc]))}
+                 :change-on-blur? false
+                 :placeholder     "search document"]
+               [re-com/label
+                 :label (<sub [::subs/search-matches])]]])
 
 (defn doc-display
   []
   [re-com/v-box
-   :height
-   "300px"
-   :children
-   [[tav/doc-header (<sub [::subs/selected-doc])]
-    [re-com/h-split
-     :src
-     (at)
-     :initial-split
-     85
-     :panel-1
-     [tav/editor (<sub [::subs/selected-doc])]
-     :panel-2
-     [tav/annotation-info]]]])
+    :height   "300px"
+    :children [[tav/doc-header (<sub [::subs/selected-doc])]
+               [re-com/h-split
+                 :src           (at)
+                 :initial-split 85
+                 :panel-1       [tav/editor (<sub [::subs/selected-doc])]
+                 :panel-2       [tav/annotation-info]]]])
 
 (defn annotation-panel
   []
   [re-com/v-box
-   :src
-   (at)
-   :children
-   [[undo-controls]
-    [re-com/h-box :children [[tac/profile-controls] [tac/doc-controls]]]
-    [re-com/h-box :children [[color-controls] [tac/ann-controls]]]
-    [re-com/h-split
-     :src
-     (at)
-     :initial-split
-     25
-     :panel-1
-     [owl/owl-hierarchies]
-     :panel-2
-     [doc-display]]
-    [re-com/h-box :children [[tac/span-controls] [search-controls]]]]])
+    :src      (at)
+    :children [[undo-controls]
+               [re-com/h-box
+                 :children [[tac/profile-controls] [tac/doc-controls]]]
+               [re-com/h-box
+                 :children [[color-controls] [tac/ann-controls]]]
+               [re-com/h-split
+                 :src           (at)
+                 :initial-split 25
+                 :panel-1       [owl/owl-hierarchies]
+                 :panel-2       [doc-display]]
+               [re-com/h-box
+                 :children [[tac/span-controls] [search-controls]]]]])
 
 (defn filter-controls
   []
   (let [showing? (r/atom false)]
-    [:div
-     {:style {:position :fixed
-              :top      0
-              :right    0
-              :z-index  1}}
-     [re-com/popover-anchor-wrapper
-      :showing?
-      showing?
-      :position
-      :below-left
-      :anchor
-      [re-com/button
-       :on-click
-       (re-com/handler-fn (swap! showing? not))
-       :label
-       "Filters"]
-      :popover
-      [re-com/popover-content-wrapper
-       :title
-       "Filters"
-       :body
-       [review/filterer]]]]))
+    [:div {:style {:position :fixed
+                   :top      0
+                   :right    0
+                   :z-index  1}}
+      [re-com/popover-anchor-wrapper
+        :showing? showing?
+        :position :below-left
+        :anchor   [re-com/button
+                    :on-click (re-com/handler-fn (swap! showing? not))
+                    :label    "Filters"]
+        :popover  [re-com/popover-content-wrapper
+                    :title "Filters"
+                    :body  [review/filterer]]]]))
 
 
 (defn review-panel
   []
   [:div
-   [re-com/title :src (at) :label "Review" :level :level1]
-   [review/chooser]
-   [review/table]
-   [filter-controls]])
+   [re-com/title
+     :src   (at)
+     :label "Review"
+     :level :level1] [review/chooser] [review/table] [filter-controls]])
 
 (defn graph-panel
   []
   [:div
    [re-com/v-box
-    :children
-    [[owl/owl-controls]
-     [ra-controls/graph-space-controls
-      [::ra-subs/graph-spaces]
-      [::ra-subs/selected-graph-space-id]
-      #(>evt [::ra-evts/select-graph-space %])]
-     [ra-controls/node-controls]
-     [ra-controls/edge-controls]
-     [re-com/h-box
-      :children
-      [[owl/owl-obj-prop-hierarchy]
-       [ra/graph
-        :relation-annotation-graph
-        [::ra-subs/selected-realized-graph]
-        [::ra-subs/selected-graph-space-id]]]]]]])
+     :children [[owl/owl-controls]
+                [ra-controls/graph-space-controls [::ra-subs/graph-spaces]
+                 [::ra-subs/selected-graph-space-id]
+                 #(>evt [::ra-evts/select-graph-space %])]
+                [ra-controls/node-controls] [ra-controls/edge-controls]
+                [re-com/h-box
+                  :children [[owl/owl-obj-prop-hierarchy]
+                             [ra/graph
+                               :relation-annotation-graph
+                               [::ra-subs/selected-realized-graph]
+                               [::ra-subs/selected-graph-space-id]]]]]]])
 
 (defn analogy-panel
   []
   [re-com/v-box
-   :size
-   "1 1 100%"
-   :children
-   [[ra-controls/graph-space-controls
-     [::ana-subs/analogy-graphs]
-     [::ana-subs/selected-analogy-graph-id]
-     #(>evt [::ana-evts/select-graph-id %])]
-    [ra-controls/node-controls]
-    [ra-controls/edge-controls]
-    [ana-views/perform-analogy-button]
-    [re-com/h-box
-     :size
-     "1 1 100%"
-     :children
-     [(map (fn [e] (with-meta e {:key (str (random-uuid))}))
-           (interpose [re-com/line :size "4px" :color "black"]
-            (for [graph-panel (<sub [::ana-subs/graph-panels])]
-              [ana-views/analogy-graph graph-panel])))]]
-    [ana-views/add-graph-panel-button]]])
+    :size     "1 1 100%"
+    :children [[ra-controls/graph-space-controls [::ana-subs/analogy-graphs]
+                [::ana-subs/selected-analogy-graph-id]
+                #(>evt [::ana-evts/select-graph-id %])]
+               [ra-controls/node-controls] [ra-controls/edge-controls]
+               [ana-views/perform-analogy-button]
+               [re-com/h-box
+                 :size "1 1 100%"
+                 :children
+                 [(map (fn [e] (with-meta e {:key (str (random-uuid))}))
+                       (interpose [re-com/line
+                                    :size  "4px"
+                                    :color "black"]
+                        (for [graph-panel (<sub [::ana-subs/graph-panels])]
+                          [ana-views/analogy-graph graph-panel])))]]
+               [ana-views/add-graph-panel-button]]])
 
 (defmethod routes/panels :review-panel [] [review-panel])
 (defmethod routes/panels :annotation-panel [] [annotation-panel])
@@ -249,9 +215,6 @@
   []
   (let [active-panel (rf/subscribe [::subs/active-panel])]
     [re-com/v-box
-     :src
-     (at)
-     :height
-     "100%"
-     :children
-     [[page-title] (routes/panels @active-panel)]]))
+      :src      (at)
+      :height   "100%"
+      :children [[page-title] (routes/panels @active-panel)]]))
